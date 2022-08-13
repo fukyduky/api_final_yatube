8e82a0e54e5cdf3238b90b2e379514fd5f18a452
Evgeniy Gruzintcev <boolbashka@ya.ru>
2022-08-14 02:16:31 +0300

    второй коммит на проверку

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,7 +5,6 @@ from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 v1_router = DefaultRouter()
 v1_router.register('posts', PostViewSet, basename='posts')
-v1_router.register('follow', FollowViewSet, basename='follow')
 v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 v1_router.register('groups', GroupViewSet)
@@ -14,4 +13,5 @@ urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path(r'v1/auth/', include('djoser.urls')),
     path(r'v1/', include('djoser.urls.jwt')),
+    path('v1/follow/', FollowViewSet.as_view()),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Group, Post, User
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework import generics
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
@@ -36,24 +36,18 @@ class CommentViewSet(viewsets.ModelViewSet):
         return post.comments.all()
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
-
-    def create(self, request):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    permission_classes = (AllowAny,)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+class FollowViewSet(generics.ListCreateAPIView):
     serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ['user__username', 'following__username']
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.request.user.username)
         return user.follower
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
